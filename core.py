2cfdb03f960f11024bd866be865e233468f86896
ZanSara <chinasaz@yandex.com>
2015-05-14 12:35:03 +0800

    scratch of the uploading process

--- core.py
+++ core.py
@@ -1,14 +1,21 @@
-from flask import Flask, request, url_for, send_from_directory
+from flask import Flask, request, url_for, redirect, send_from_directory
 from jinja2 import Environment, PackageLoader
+from werkzeug import secure_filename
+import sqlite3
+import json
+import os
 
-import os.path
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'tiff'])
+DATABASE_PATH = 'base.sqlite'
 
 env = Environment(loader=PackageLoader('core', '/templates'))
 
 app = Flask(__name__, static_folder="static")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app.secret_key = ".ASF\x89m\x14\xc9s\x94\xfaq\xca}\xe1/\x1f3\x1dFx\xdc\xf0\xf9"
+app.secret_key = ".ASF\x89m\x14\xc9s\x94ff\xfaq\xca}h\xe1/\x1f3\x1dFxj\xdc\xf0\xf9"
 
 
 
@@ -17,24 +24,24 @@ def style(style):
 		styles of different sections of the website (css, boxes order)"""
 	if style=="home":
 		var = {"sidebar":["base/boxrif.html", "base/boxprog.html", "base/boxsez.html", "base/boxweb.html"]};
-		var["url_for_css"] = "static/css/style-home.css"
+		var["url_for_css"] = "/static/css/style-home.css"
 		
 	elif style=="rifugio":
 		var = {"sidebar":["base/boxrif.html", "base/boxprog.html", "base/boxsez.html", "base/boxweb.html"]};
-		var["url_for_css"] = "static/css/style-rifugio.css"
+		var["url_for_css"] = "/static/css/style-rifugio.css"
 		
 	elif style=="programmi":
 		var = {"sidebar":["base/boxprog.html", "base/boxrif.html", "base/boxsez.html", "base/boxweb.html"]};
-		var["url_for_css"] = "static/css/style-programmi.css"
+		var["url_for_css"] = "/static/css/style-programmi.css"
 		
 	elif style=="sezione":
 		var = {"sidebar":["base/boxsez.html", "base/boxrif.html", "base/boxprog.html", "base/boxweb.html"]};
-		var["url_for_css"] = "static/css/style-sezione.css"
+		var["url_for_css"] = "/static/css/style-sezione.css"
 		
 	elif style=="webmaster":
 		var = {"sidebar":["base/boxweb.html", "base/boxrif.html", "base/boxprog.html", "base/boxsez.html"]};
-		var["url_for_css"] = "static/css/style-webmaster.css"
-		
+		var["url_for_css"] = "/static/css/style-webmaster.css"
+
 	return var;
 	
 
@@ -140,12 +147,6 @@ def webhome():
 	template = env.get_template("web-home.html")
 	return template.render(var)
 	
-@app.route("/pannello/area-riservata" , methods=["GET"])
-def webres():
-	var = style("webmaster")
-	template = env.get_template("web-reserved.html")
-	return template.render(var)
-
 @app.route("/pannello/feedback" , methods=["GET"])
 def webfed():
 	var = style("webmaster")
@@ -157,6 +158,130 @@ def webmaster():
 	var = style("webmaster")
 	template = env.get_template("web-master.html")
 	return template.render(var)
+
+
+
+# ************ Reserved Area *******************************************
+
+def allowed_file(filename):
+	""" Checks for the file extension to be one of the allowed ones """
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+	
+@app.route('/uploads/<filename>')
+def static_file(filename):
+	""" Serves static files """
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route("/pannello/area-riservata/login", methods=["GET", "POST"])
+def weblogin():
+	var = style("webmaster")
+
+	if request.method == "GET":
+		template = env.get_template("web-res-login.html")
+		var["login"]=1
+		return template.render(var)
+		
+	if request.method == "POST":
+		print request.form["user"]
+		print request.form["password"]
+		log = login(request.form["user"], request.form["password"])
+		if not log:
+			var['msg'] = "Username o password scorretti!"
+			template = env.get_template("web-res-login.html")
+			return template.render(var)
+		else:
+			return redirect(url_for('webres'))
+		return ":/"
+
+
+
+@app.route("/pannello/area-riservata", methods=["GET", "POST"])
+def webres():
+#	if not session.get('logged_in'):
+#		login()	
+	var = style("webmaster")
+	template = env.get_template("web-res-home.html")
+	return template.render(var)
+	
+
+
+@app.route("/pannello/area-riservata/upload/<obj>", methods=["GET", "POST"])
+def webupload(obj):
+#	if not session.get('logged_in'):
+#		login()	
+	var = style("webmaster")
+	var['obj']=obj
+	
+	# Bozza hardcodata - RIMEDIARE!
+	if request.method == 'POST':
+		
+		if obj=="news":
+			title = request.form['titolo']
+			text = request.form['testo']
+			file1 = request.files['foto1']
+			file2 = request.files['foto2']
+			file3 = request.files['foto3']
+			file4 = request.files['foto4']
+			file5 = request.files['foto5']
+			cap1 = request.form['descrizione1']
+			cap2 = request.form['descrizione2']
+			cap3 = request.form['descrizione3']
+			cap4 = request.form['descrizione4']
+			cap5 = request.form['descrizione5']
+			# Database call HERE
+			conn = sqlite3.connect(DATABASE_PATH)
+			cursor = conn.cursor()
+			pics, caps = [], []
+			# Se non trova il file, il bottone non funziona
+			for fil in [file1, file2, file3, file4, file5]:
+				if fil: #and allowed_file(fil.filename):
+					filename = secure_filename(fil.filename)
+					fil.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+					pics.append(filename)
+			for cap in [cap1, cap2, cap3, cap4, cap5]:
+				if cap:
+					caps.append(cap)
+				else:
+					caps.append(' ')
+			full = zip(pics, caps)
+			pic_list = json.dumps(full)  # Per decodificare, json.load(jsonyfied_var)
+			cursor.execute("INSERT INTO news (title, text, pics) VALUES (?, ?, ?)", [title, text, pic_list])
+			conn.commit()
+			
+			var["upload"]='success'
+			template = env.get_template("web-res-upload.html")
+			return template.render(var)
+			
+		if obj=='note':
+			text = request.form['testo']
+			# Database call HERE
+			conn = sqlite3.connect(DATABASE_PATH)
+			cursor = conn.cursor()
+			cursor.execute("INSERT INTO notes VALUE ?", text)
+			conn.commit()
+			conn.close()
+			
+	template = env.get_template("web-res-upload.html")
+	return template.render(var)
+	
+
+	
+#*** ERROR HANDLERS ****************
+#@app.errorhandler(404)
+#def page_not_found(e):
+	#return render_template('404.html'), 404
+#@app.errorhandler(401)
+#def unauthorized(e):
+	#return render_template('401.html'), 401
+#@app.errorhandler(500)
+#def internal_error(e):
+	#return render_template('500.html'), 500
+
+
+
+
 
 
 
---
+++ session.py
@@ -0,0 +1,12 @@
+	
+# PLACEHOLDER!!! Ci devo mettere un vero login qui
+def login(user, pwd):
+	if user=="me" and pwd=="you":
+		return 1
+	return 0
+	
+
+# PLACEHOLDER!!! Ci devo mettere un vero logout qui
+def logout():
+	#session.pop("logged_in", None)
+	return 0
